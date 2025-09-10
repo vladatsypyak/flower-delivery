367cae4e1b437e46e34247783870d40e6c179481
Vladyslava <vladatsyupyak@gmail.com>
2025-09-10 14:51:17 +0200

    Setup Express server and MongoDB connection

--- back/index.js
+++ back/index.js
@@ -1,13 +1,29 @@
-const express = require("express");
-const app = express();
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+dotenv.config();
+
+const app = express();
 app.use(express.json());
+
+const startServer = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("MongoDB connected");
+
+        const PORT = process.env.PORT || 3000;
+        app.listen(PORT, () => {
+            console.log(`Server is running on http://localhost:${PORT}`);
+        });
+    } catch (err) {
+        console.error("DB connection error:", err.message);
+        process.exit(1);
+    }
+};
+
+startServer();
 
 app.get("/", (req, res) => {
     res.send("Hello, Express!");
-});
-
-const PORT = 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
 });
