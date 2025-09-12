96be4484742922cc9e65f1ac866e73c05c8094ba
Vladyslava <vladatsyupyak@gmail.com>
2025-09-12 11:36:38 +0200

    fix: show selected shop by default

--- front/src/pages/Shops.tsx
+++ front/src/pages/Shops.tsx
@@ -1,9 +1,8 @@
-import React, { useState, useEffect } from "react";
+import React, {useState, useEffect} from "react";
 import ShopList from "../components/ShopList/ShopList";
 import FlowerCard from "../components/FlowerCard/FlowerCard";
 import {Product} from "../types";
 import {useCart} from "../hooks/useCart";
-import Cart from "../components/Cart/Cart";
 
 export interface Shop {
     _id: string;
@@ -16,18 +15,19 @@ export interface Shop {
 
 
 const ShopPage: React.FC = () => {
-    const { addToCart } = useCart();
+    const {addToCart} = useCart();
 
     const [shops, setShops] = useState<Shop[]>([]);
     const [loading, setLoading] = useState(true);
     const [selectedShop, setSelectedShop] = useState(shops[0]);
-   const [flowers, setFlowers] = useState<Product[]>([])
+    const [flowers, setFlowers] = useState<Product[]>([])
     useEffect(() => {
         fetch("http://localhost:3000/api/shops")
             .then((res) => res.json())
             .then((data) => {
                 console.log(data)
                 setShops(data);
+                setSelectedShop(data[0])
                 setLoading(false);
             })
             .catch((err) => {
@@ -36,7 +36,7 @@ const ShopPage: React.FC = () => {
             });
     }, []);
     useEffect(() => {
-        if(selectedShop){
+        if (selectedShop) {
             fetch(`http://localhost:3000/api/products/${selectedShop?._id}`)
                 .then((res) => res.json())
                 .then((data) => {
@@ -51,23 +51,25 @@ const ShopPage: React.FC = () => {
         }
 
     }, [selectedShop]);
-    const handleAddToCart = (flower: string) => {
-        console.log(`${flower} added to cart!`);
-    };
 
     return (
         <div className="flex p-4 gap-6 ">
-            <ShopList shops={shops} selectedShop={selectedShop} onSelect={setSelectedShop} />
-            <div className="flex flex-wrap gap-4 overflow-y-auto">
-                {flowers?.map((flower) => (
-                    <FlowerCard
-                        key={flower.name + Math.random()}
-                        name={flower.name}
-                        item={flower}
-                        onAddToCart={() => addToCart(flower)}
-                    />
-                ))}
-            </div>
+            {loading ? "Loading..." :
+                <>
+                    <ShopList shops={shops} selectedShop={selectedShop} onSelect={setSelectedShop}/>
+                    <div className="flex flex-wrap gap-4 overflow-y-auto">
+                        {flowers?.map((flower) => (
+                            <FlowerCard
+                                key={flower.name + Math.random()}
+                                name={flower.name}
+                                item={flower}
+                                onAddToCart={() => addToCart(flower)}
+                            />
+                        ))}
+                    </div>
+                </>
+            }
+
         </div>
     );
 };
